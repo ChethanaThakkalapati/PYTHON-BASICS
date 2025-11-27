558700720febe25b8fced299899edee41df70b0a
Chethana Thakkalapti <chethanathakkelapati@gmail.com>
2025-11-27 07:51:55 +0530

    Refactor travel distance program for clarity

--- python assigment qsn1.py
+++ python assigment qsn1.py
@@ -1,4 +1,4 @@
-'''1.	Create a python program that asks the user how far they want to travel. If they want to travel less than three miles tell them to ride Bicycle. if they want to travel more than three miles, but less than three hundred miles, tell them to ride Motor-Cycle. if they want to travel three hundred miles or more tell them to drive Super-Car'''
+'''Create a python program that asks the user how far they want to travel. If they want to travel less than three miles tell them to ride Bicycle. if they want to travel more than three miles, but less than three hundred miles, tell them to ride Motor-Cycle. if they want to travel three hundred miles or more tell them to drive Super-Car'''
 
 
 distance=int(input("enter the distance that is to be travelled: "))
@@ -8,4 +8,5 @@ elif(distance>=3 and distance<300):
     print("Use motorcycle")
 else:
     print("Use super car")
+
 
