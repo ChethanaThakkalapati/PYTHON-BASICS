b01ea6f3b5d6146ce702c5a68da1f8e988d006d3
Chethana Thakkalapti <chethanathakkelapati@gmail.com>
2025-11-27 07:41:03 +0530

    Refactor distance input program with description
    Updated the code to include a description of the program's functionality.

--- python assigment qsn1.py
+++ python assigment qsn1.py
@@ -1,3 +1,6 @@
+'''1.	Create a python program that asks the user how far they want to travel. If they want to travel less than three miles tell them to ride Bicycle. if they want to travel more than three miles, but less than three hundred miles, tell them to ride Motor-Cycle. if they want to travel three hundred miles or more tell them to drive Super-Car'''
+
+
 distance=int(input("enter the distance that is to be travelled: "))
 if(distance<3):
     print("Use bicycle")
@@ -5,3 +8,4 @@ elif(distance>=3 and distance<300):
     print("Use motorcycle")
 else:
     print("Use super car")
+
