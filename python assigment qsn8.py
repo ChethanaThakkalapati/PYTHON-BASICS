945b4465a898e82ed4362679b94bf8b8599b71da
Chethana Thakkalapti <chethanathakkelapati@gmail.com>
2025-11-27 07:50:30 +0530

    Fix indentation and formatting in qsn8.py

--- python assigment qsn8.py
+++ python assigment qsn8.py
@@ -1,4 +1,4 @@
-'''8.	Output dictionary which contains only the odd numbers that are present in the input list as keys and their cubes as values using List Comprehension'''
+'''Output dictionary which contains only the odd numbers that are present in the input list as keys and their cubes as values using List Comprehension'''
 a = input("Enter numbers separated by spaces: ")
 numbers = list(map(int, a.split()))
 
@@ -8,4 +8,5 @@ odd_cubes = {n: n**3 for n in numbers if n % 2 != 0}
 # Display the result
 print("Dictionary of odd numbers and their cubes:")
 print(odd_cubes)
+
 
