4411e4098ee79757a9ea60ceca8b3c2a02784c3a
Chethana Thakkalapti <chethanathakkelapati@gmail.com>
2025-11-27 07:42:06 +0530

    Fix formatting and re-add cost calculation code

--- python assigment qsn2.py
+++ python assigment qsn2.py
@@ -1,3 +1,4 @@
+'''2.	An App for Mobile hosting provider charges- $0.51 per hour how much does it cost to operator per day, per week, per month? how many days can i operate one server with$918?'''
 cost_hour = 0.51
 
 cost_day = cost_hour * 24
@@ -13,3 +14,4 @@ print("Cost to operate per day: $", cost_day)
 print("Cost to operate per week: $", cost_week)
 print("Cost to operate per month: $", cost_month)
 print("Days you can operate with $918:", days_possible)
+
