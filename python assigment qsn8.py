9f619114f0d2efc54f52cd7327644506b72afead
Chethana Thakkalapti <chethanathakkelapati@gmail.com>
2025-11-27 07:48:55 +0530

    Fix formatting and clean up input handling

--- python assigment qsn8.py
+++ python assigment qsn8.py
@@ -1,3 +1,4 @@
+'''8.	Output dictionary which contains only the odd numbers that are present in the input list as keys and their cubes as values using List Comprehension'''
 a = input("Enter numbers separated by spaces: ")
 numbers = list(map(int, a.split()))
 
@@ -7,3 +8,4 @@ odd_cubes = {n: n**3 for n in numbers if n % 2 != 0}
 # Display the result
 print("Dictionary of odd numbers and their cubes:")
 print(odd_cubes)
+
